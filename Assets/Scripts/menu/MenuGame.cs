18e4bde62af5b84d5283ca278011ee9974d21365
muriloorias <mu.a.o@outlook.com>
2025-09-18 21:12:02 -0300

    adicionando menu

---
+++ Assets/Scripts/menu/MenuGame.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class MenuGame : MonoBehaviour
+{
+    public GameObject pauseMenuUi;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;          
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUi.SetActive(true); 
+        Time.timeScale = 0f;          
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Saindo...");
+        Application.Quit(); 
+    }
+}
